7d32d9312f51f02bbc3fd6b77eb9ae80005305ec
StealiesTag <jeremiahmonebit17@gmail.com>
2026-09-07 10:53:53 +0000

    Read test.mp4, will resume later at local desktop.

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 import cv2
-import cvzone
 from ultralytics import YOLO
 
 model = YOLO('yolo12n.pt')
 names=model.names
+
+cap = cv2.VideoCapture('video/test.mp4')
+
+while True:
+    success, img = cap.read()
+    if not success:
+        break
+
+    cv2.imshow('TapoVision', img)
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+    
+cap.release()
+cv2.destroyAllWindows()
